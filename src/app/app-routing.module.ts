882d7b75cc3a9cdfb5847c490796379c1595e580
Yossi Itzinger <yossi@alluresecurity.com>
2019-06-04 19:45:09 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CurrentEventsComponent } from './Components/current-events/current-events.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ServicesComponent } from './Components/services/services.component';
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { AboutComponent } from './Components/about/about.component';
+import { DonateComponent } from './Components/donate/donate.component';
+import { from } from 'rxjs';
+
+const routes: Routes = [
+  { path: 'current', component: CurrentEventsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'donate', component: DonateComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// bootstrap
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { CurrentEventsComponent } from './Components/current-events/current-events.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ServicesComponent } from './Components/services/services.component';
+import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { AboutComponent } from './Components/about/about.component';
+import { DonateComponent } from './Components/donate/donate.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CurrentEventsComponent,
+    HomeComponent,
+    ServicesComponent,
+    SignUpComponent,
+    AboutComponent,
+    DonateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
