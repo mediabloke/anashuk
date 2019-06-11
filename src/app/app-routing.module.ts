914d1510010d7d7d5691dd76c83ad02b1af780ef
Yossi Itzinger <yossi@alluresecurity.com>
2019-06-11 18:39:43 -0400

    push for initial push to host

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { CurrentEventsComponent } from './Components/current-events/current-events.component';
-import { HomeComponent } from './Components/home/home.component';
 import { ServicesComponent } from './Components/services/services.component';
 import { SignUpComponent } from './Components/sign-up/sign-up.component';
 import { AboutComponent } from './Components/about/about.component';
 import { DonateComponent } from './Components/donate/donate.component';
+import { HomeComponent } from './Components/home/home.component';
 import { from } from 'rxjs';
 
 const routes: Routes = [
   { path: 'current', component: CurrentEventsComponent },
-  { path: 'home', component: HomeComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'about', component: AboutComponent },
   { path: 'donate', component: DonateComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
